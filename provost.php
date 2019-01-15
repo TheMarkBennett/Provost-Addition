65efbcbe8b1366f0eba130e37a274d76802f51c2
Mark Bennett <mark.bennett@ucf.edu>
2019-01-15 09:48:39 -0500

    initial release

---
+++ provost.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Plugin Name: Provost Website Aditions
+Plugin URI:   https://ucf.edu
+Description:  Adds features for the provost website
+Version:      1.0
+Author:       Mark Bennett
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  ucf
+Domain Path:  /languages
+*/
+
+
+
+define('UCF_PRO_PATH', plugin_dir_path(__FILE__));
+define('UCF_PRO_NAME', plugin_basename(__FILE__));
+
+
+require_once(UCF_PRO_PATH . 'inc/login.php');
